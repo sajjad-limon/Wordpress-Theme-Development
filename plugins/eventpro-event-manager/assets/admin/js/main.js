03d850716d46b1ffa3b9b7a1b78f6a6f90696f6a
Limon Hossain <122174379+limondevwp@users.noreply.github.com>
2023-01-08 22:32:02 +0600

    Add files via upload

---
+++ plugins/eventpro-event-manager/assets/admin/js/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function () {
+  jQuery("#evm_date").datepicker({
+    dateFormat: "d",
+  });
+});
+
+jQuery(document).ready(function () {
+  jQuery("#evm_started").datepicker({
+    dateFormat: "M",
+  });
+});
+
